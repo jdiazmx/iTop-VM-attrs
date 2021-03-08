41933ac43ddf2c97f74dbd0040f29de8f5aea321
Jorge Díaz <jorge.diaz@gmail.com>
2021-03-08 01:13:42 -0600

    Add spanish dictionary.

---
+++ schirrms-vm-attrs/es_cr.dict.schirrms-vm-attrs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellaño', array(
+	// Dictionary entries go here
+	'Class:VirtualMachine/Attribute:S_UUID' => 'Servidor UUID',
+	'Class:VirtualMachine/Attribute:S_FUNCTION' => 'Función principal del servidor',
+	'Class:VirtualMachine/Attribute:S_CREATOR' => 'Creador del servidor',
+	'Class:VirtualMachine/Attribute:S_Contact_1' => 'Primer contacto',
+	'Class:VirtualMachine/Attribute:S_Contact_2' => 'Segundo contacto',
+	'Class:VirtualMachine/Attribute:S_EndOfLife' => 'Fecha prevista de fin de vida útil',
+	'Server:Operational' => 'Información operativa',
+	'Class:VirtualMachine/Attribute:S_LastBootDate' => 'Última fecha de inicio',
+	'Class:VirtualMachine/Attribute:S_CurrentHypervisor' => 'Alojado por',
+	'Class:VirtualMachine/Attribute:S_OS_Version' => 'Versión de SO',
+	'Class:VirtualMachine/Attribute:S_OS_Distributor' => 'Distribuidor de SO',
+	'Class:VirtualMachine/Attribute:netbackup_version' => 'Versión de NetBackup',
+	'Class:VirtualMachine/Attribute:location_id' => 'Localización',
+	'Class:VirtualMachine/Attribute:S_Comment' => 'Comentarios',
+	'Class:VirtualMachine/Attribute:S_Usage' => 'Tipo de uso',
+	'Class:VirtualMachine/Attribute:S_Project' => 'Miembro del Proyecto',
+	'Class:VirtualMachine/Attribute:S_Template' => 'Plantilla original',
+	'Class:VirtualMachine/Attribute:S_Backup' => 'Información de respaldo',
+	'Class:VirtualMachine/Attribute:power_status' => 'Estado de energía de la máquina virtual',
+	'Class:VirtualMachine/Attribute:vcenter' => 'VCenter',
+	'Class:lnkVirtualDeviceToVolume/Attribute:virtual_volume_name' => 'Nombre en el sistema virtual', 
+));
+?>
